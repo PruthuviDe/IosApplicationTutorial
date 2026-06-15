2e4d6368ac10cc0b7f1c045a9eed68b211761ada
PruthuviDe <pruthuvide@outlook.com>
2026-06-15 21:39:39 +0530

    feat: build basic Tap Frenzy gameplay

--- IosApplicationTutorial/ContentView.swift
+++ IosApplicationTutorial/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(spacing: 30) {
+            
+            Text("Tap Frenzy")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Score: \(score)")
+                .font(.title)
+            
+            Button(action: {
+                score += 1
+            }) {
+                
+                Text("TAP!!")
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 250)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+            }
         }
         .padding()
     }
