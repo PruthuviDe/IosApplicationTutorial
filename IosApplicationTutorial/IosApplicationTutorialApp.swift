49d9898d45518a6103d2ca559d0e885fed04b3fe
PruthuviDe <pruthuvide@outlook.com>
2026-06-15 21:17:18 +0530

    Initial Commit

---
+++ IosApplicationTutorial/IosApplicationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosApplicationTutorialApp.swift
+//  IosApplicationTutorial
+//
+//  Created by Pruthuvi de Silva on 2026-06-15.
+//
+
+import SwiftUI
+
+@main
+struct IosApplicationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
