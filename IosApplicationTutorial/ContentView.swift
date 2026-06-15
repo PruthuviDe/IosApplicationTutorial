0144910642e992487774978b46eb40fa52d75033
PruthuviDe <pruthuvide@outlook.com>
2026-06-15 21:43:54 +0530

    feat: add countdown timer

--- IosApplicationTutorial/ContentView.swift
+++ IosApplicationTutorial/ContentView.swift
@@ -6,26 +6,35 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    
+
     @State private var score = 0
-    
+    @State private var timeRemaining = 10
+
+    // Countdown timer
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        
+
         VStack(spacing: 30) {
-            
+
             Text("Tap Frenzy")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            
+
             Text("Score: \(score)")
                 .font(.title)
-            
+
+            Text("Time: \(timeRemaining)")
+                .font(.title2)
+                .foregroundColor(.orange)
+
             Button(action: {
                 score += 1
             }) {
-                
+
                 Text("TAP!!")
                     .font(.system(size: 40, weight: .bold))
                     .foregroundColor(.white)
@@ -35,6 +44,17 @@ struct ContentView: View {
             }
         }
         .padding()
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .center
+        )
+        
+        .onReceive(timer) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
     }
 }
 
