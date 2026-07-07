7a63f9ca1b84dcee27d129d4446dfc0eefaf4885
PruthuviDe <pruthuvide@outlook.com>
2026-07-07 08:46:09 +0530

    feat(model): add imageName property to GameMode enum
    Added a computed imageName property to GameMode that returns the
    corresponding asset catalog image name for each game mode:
      - .tapFrenzy  -> 'tap_frenzy'
      - .lightItUp  -> 'light_it_up'
      - .quizRush   -> 'quiz_rush'
    
    This provides a single source of truth for image lookups and
    eliminates redundant switch statements across views.

--- IosApplicationTutorial/Models/GameMode.swift
+++ IosApplicationTutorial/Models/GameMode.swift
@@ -13,11 +13,19 @@ enum GameMode: String, Codable, CaseIterable {
         }
     }
 
+    var imageName: String {
+        switch self {
+        case .tapFrenzy: return "tap_frenzy"
+        case .lightItUp: return "light_it_up"
+        case .quizRush:  return "quiz_rush"
+        }
+    }
+
     var accentColor: Color {
         switch self {
-        case .tapFrenzy: return .red
-        case .lightItUp: return .cyan
-        case .quizRush:  return .purple
+        case .tapFrenzy: return Color(red: 0.92, green: 0.26, blue: 0.35)
+        case .lightItUp: return Color(red: 0.20, green: 0.83, blue: 0.95)
+        case .quizRush:  return Color(red: 0.65, green: 0.35, blue: 0.95)
         }
     }
 
